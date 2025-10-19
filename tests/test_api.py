bc1f7a1572e105a81a0b335d5754188995c7bdb3
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-10-19 17:46:55 -0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,87 +1,90 @@
+from . import app
+import os
 import json
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 
-def test_health(client):
-    res = client.get("/health")
-    assert res.status_code == 200
-
-
-def test_count(client):
-    res = client.get("/count")
-    assert res.status_code == 200
-    assert res.json['length'] == 10
-
-
-def test_data_contains_10_pictures(client):
-    res = client.get("/picture")
-    assert len(res.json) == 10
-
-
-def test_get_picture(client):
-    res = client.get("/picture")
-    assert res.status_code == 200
-    assert len(res.json) == 10
-
-
-def test_get_pictures_check_content_type_equals_json(client):
-    res = client.get("/picture")
-    assert res.headers["Content-Type"] == "application/json"
-
-
-def test_get_picture_by_id(client):
-    id_delete = 2
-    res = client.get(f'/picture/{id_delete}')
-    assert res.status_code == 200
-    assert res.json['id'] == id_delete
-
-    res = client.get('/picture/404')
-    assert res.status_code == 404
-
-
-def test_pictures_json_is_not_empty(client):
-    res = client.get("/picture")
-    assert len(res.json) > 0
-
-
-def test_post_picture(picture, client):
-    # create a brand new picture to upload
-    res = client.post("/picture", data=json.dumps(picture),
-                      content_type="application/json")
-    assert res.status_code == 201
-    assert res.json['id'] == picture['id']
-    res = client.get("/count")
-    assert res.status_code == 200
-    assert res.json['length'] == 11
-
-def test_post_picture_duplicate(picture, client):
-    # create a brand new picture to upload
-    res = client.post("/picture", data=json.dumps(picture),
-                      content_type="application/json")
-    assert res.status_code == 302
-    assert res.json['Message'] == f"picture with id {picture['id']} already present"
-
-def test_update_picture_by_id(client, picture):
-    id = '2'
-    res = client.get(f'/picture/{id}')
-    res_picture = res.json
-    assert res_picture['id'] == 2
-    res_state = res_picture["event_state"]
-    new_state = "*" + res_state
-    res_picture["event_state"] = new_state
-    res = client.put(f'/picture/{id}', data=json.dumps(res_picture),
-                     content_type="application/json")
-    res.status_code == 200
-    res = client.get(f'/picture/{id}')
-    assert res.json['event_state'] == new_state
-
-def test_delete_picture_by_id(client):
-    res = client.get("/count")
-    assert res.json['length'] == 11
-    res = client.delete("/picture/1")
-    assert res.status_code == 204
-    res = client.get("/count")
-    assert res.json['length'] == 10
-    res = client.delete("/picture/100")
-    assert res.status_code == 404
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
+data: list = json.load(open(json_url))
 
 
 
+######################################################################
+# RETURN HEALTH OF THE APP
+######################################################################
+
+
+@app.route("/health")
+def health():
+    return jsonify(dict(status="OK")), 200
+
+######################################################################
+# COUNT THE NUMBER OF PICTURES
+######################################################################
+
+
+@app.route("/count")
+def count():
+    """return length of data"""
+    if data:
+        return jsonify(length=len(data)), 200
+
+    return {"message": "Internal server error"}, 500
+
+
+######################################################################
+# GET ALL PICTURES
+######################################################################
+@app.route("/picture", methods=["GET"])
+def get_pictures():
+    return jsonify(data), 200 
+
+######################################################################
+# GET A PICTURE
+######################################################################
+
+
+@app.route("/picture/<int:id>", methods=["GET"])
+def get_picture_by_id(id):
+
+    for dic in data: 
+        if dic["id"] == id: 
+            return jsonify(dic), 200 
+    
+    return jsonify({"message":'No valid number'}), 404
+
+    
+
+
+######################################################################
+# CREATE A PICTURE
+######################################################################
+@app.route("/picture", methods=["POST"])
+def create_picture():
+    
+    userData = request.get_json()
+    for dic in data: 
+        if dic['id'] == userData['id']: 
+            userData['Message'] = f"picture with id {userData['id']} already present"
+            return jsonify(userData), 302
+    
+    data.append(userData)
+    return jsonify(userData), 201 
+
+    
+
+######################################################################
+# UPDATE A PICTURE
+######################################################################
+
+
+@app.route("/picture/<int:id>", methods=["PUT"])
+def update_picture(id):
+    pass
+
+######################################################################
+# DELETE A PICTURE
+######################################################################
+@app.route("/picture/<int:id>", methods=["DELETE"])
+def delete_picture(id):
+    pass
